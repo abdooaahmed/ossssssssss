f18762bf048f2f9bf5c0fabc7251f395b736deb7
abdooaahmed <167030413+abdooaahmed@users.noreply.github.com>
2024-04-28 05:55:39 +0300

    Add files via upload

---
+++ 111111111111111/111111111111111/Form2.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace _111111111111111
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        public TextBox txttotal;
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblgg = new Label();
+            SuspendLayout();
+            // 
+            // lblgg
+            // 
+            lblgg.AutoSize = true;
+            lblgg.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblgg.ForeColor = Color.Black;
+            lblgg.Location = new Point(207, 194);
+            lblgg.Name = "lblgg";
+            lblgg.Size = new Size(397, 46);
+            lblgg.TabIndex = 0;
+            lblgg.Text = "Grade Percentage: 0.0%";
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lblgg);
+            Name = "Form2";
+            Text = "Form2";
+            Load += Form2_Load;
+            Paint += Form2_Paint;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblgg;
+    }
+}
---
+++ 111111111111111/111111111111111/Form2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _111111111111111
+{
+    public partial class Form2 : Form
+    {
+        
+        public Form2()
+        {
+            InitializeComponent();
+
+        }
+        public int total {  get; set; }
+        private void Form2_Paint(object sender, PaintEventArgs e)
+        {
+            
+
+            double Total = total;
+            lblgg.Text = "Grade Percentage: " + total.ToString()+"%";
+          
+            Graphics g = e.Graphics;
+            Brush mybrush = new SolidBrush(Color.BlueViolet);
+
+            g.FillRectangle(mybrush, 150, 100, 500, 500);
+
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
